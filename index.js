de96d15fd6ccbc06b9cd6546f2fde62170e6aa51
Yuii-Antoniuk <u983405@gmail.com>
2026-06-06 22:27:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+"use strict";
+
+// 1
+let a = 1;
+let b = 1;
+let sum = a + b;
+
+// 2
+let c = 2;
+let d = 1;
+let riznytsya = c - d;
+
+// 3
+let e = 2;
+let x = 3;
+let dobutok = e * x;
+
+// 4
+let y = 2;
+let z = 1;
+let chastka = y / z;
+
+// 5 
+let l = 3;
+let k = 2;
+let ostacha = l % k;
+
+// 6 
+let r = 2;
+let t = 2;
+let stupin = r ** t;
+
+// 7
+let w = 7;
+let p = 4;
+let average = (w + p) / 2;
+
+// 8 
+let cm = 135;
+let m = cm / 100;
+
+// 9
+let minute = 2;
+let second = minute * 60;
+
+// 10 
+let side = 4;
+let perimeter = side * 4;
+
+// 11 
+let width = 9;
+let height = 3;
+let area = width * height;
+
+// 12
+let item1 = 20;
+let item2 = 53;
+let item3 = 78;
+let toPay = item1 + item2 + item3;
+
+// 13
+let currentYuear = 2026;
+let birthYear = 2004;
+let age = currentYuear - birthYear;
+
+// 14 
+let f = 36;
+let half = f / 2;
+
+// 15
+let g = 100;
+let third = g / 3;
+
+// 16 
+let days = 3;
+let hours = days * 24;
+
+// 17
+let Hours = 2;
+let Minutes = Hours * 60;
+
+// 18 
+let num = 47;
+let n = Math.floor(num / 10);
+let v = num % 10;          
+let Sum = n + v;        
+
+// 19
+let rad = 5;
+let length = 2 * 3.14 * rad; 
+console.log(length); 
+
+// 20 
+let Rad = 5; 
+let circleArea = 3.14 * (Rad ** 2);
